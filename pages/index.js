d5afc460cf148b8986a3286d34ebd8914baccf54
Erisan Akorede <erisanakorede@gmail.com>
2021-05-22 13:34:16 +0100

    Fixed cors origin

--- pages/index.js
+++ pages/index.js
@@ -5,47 +5,61 @@ export default function Home() {
   const [modalShow, setModalShow] = React.useState(false);
   return (
     <>
-        <Head>
-          <title>Home - Faker-NG</title>
-          <meta
-            name="description"
-            content="Generate thousand on nigerian data e.g name, state, gender e.t.c "
-          />
-          <link rel="icon" href="/images/nigeria.jpeg" />
-        </Head>
+      <Head>
+        <title>Home - Faker-NG</title>
+        <meta
+          name="description"
+          content="Generate thousand on nigerian data e.g name, state, gender e.t.c "
+        />
+        <link rel="icon" href="/images/nigeria.jpeg" />
+      </Head>
 
-        <div className="container">
-          <div className="row">
+      <div className="container">
+        <div className="row">
           <h3 className="mb-5 mt-3 text-start text-white">Eri's</h3>
 
-            <div className="col-md-7">
-              <div className="text-bg">
-                <h1>
-                  Generate Fake Nigerian
-                  <br /> Data Or Info
-                </h1>
-                <div className="summary">
-                  With thise tool, you can generate thousands on nigerian data
-                  in a JSON or list format.
-                </div>
-                <div className="clearfix"></div>
+          <div className="col-md-7">
+            <div className="text-bg">
+              <h1>
+                Generate Fake Nigerian
+                <br /> Data Or Info
+              </h1>
+              <div className="summary">
+                With thise tool, you can generate thousands on nigerian data in
+                a JSON or list format.
+              </div>
+              <div className="clearfix"></div>
 
-                <a className="btn btn-lg mt-4 text-white btn-success" href="#" onClick={() => setModalShow(true)}>
-                  Get started
-                </a>
-              </div>
+              <a
+                className="btn btn-lg mt-4 text-white btn-success"
+                href="#"
+                onClick={() => setModalShow(true)}
+              >
+                Get started
+              </a>
             </div>
-            <div className="col-md-5 overflow-hidden">
-              <div className="text-img mt-5">
-              <img className="img-fluid" src="/images/undraw_server_status_5pbv.svg" />
-               
-                <h3 className="text-bg">01</h3>
-              </div>
+          </div>
+          <div className="col-md-5 overflow-hidden">
+            <div className="text-img mt-5">
+              <img
+                className="img-fluid"
+                src="/images/undraw_server_status_5pbv.svg"
+              />
+
+              <h3 className="text-bg">01</h3>
             </div>
           </div>
         </div>
+      </div>
       <div className="clearfix my-5">
         <ModalComponent show={modalShow} onHide={() => setModalShow(false)} />
+      </div>
+
+      <div className="container text-bg">
+        Copyright {new Date().getFullYear()}
+        <div>
+        Contact: <a className="text-bg"  href="mailto:erisanakorede@gmail.com">erisanakorede@gmail.com</a>
+        </div>
       </div>
     </>
   );
